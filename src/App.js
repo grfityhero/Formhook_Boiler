07cc6abc7b4c42bf769d05ced89e9198df105641
Eli Malki <43859403+grfityhero@users.noreply.github.com>
2022-03-10 10:51:50 +0200

    up

--- src/App.js
+++ src/App.js
@@ -28,28 +28,48 @@ export default function App() {
         placeholder="Phone"
         {...register("Phone", { required: true, maxLength: 100 })}
       />
-      <select {...register("Title", { required: true })}>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
-      </select>
+
       <input
         type="text"
         placeholder="Last name"
         {...register("Last name", { required: true, maxLength: 100 })}
       />
 
-      <input
-        {...register("Developer", { required: true })}
-        type="radio"
-        value="Yes"
-      />
-      <input
-        {...register("Developer", { required: true })}
-        type="radio"
-        value="No"
-      />
+      <select {...register("Title", { required: true })}>
+        <option value="Mr">aaa</option>
+        <option value="Mrs">bbb</option>
+      </select>
+      <ul>
+        <li>
+          <input
+            {...register("Developer", { required: true })}
+            type="radio"
+            id="f-option"
+            name="selector"
+          />
+          <label for="f-option">Pizza</label>
+
+          <div class="check"></div>
+        </li>
+
+        <li>
+          <input type="radio" id="s-option" name="selector" />
+          <label for="s-option">Bacon</label>
+
+          <div class="check">
+            <div class="inside"></div>
+          </div>
+        </li>
+
+        <li>
+          <input type="radio" id="t-option" name="selector" />
+          <label for="t-option">Cats</label>
+
+          <div class="check">
+            <div class="inside"></div>
+          </div>
+        </li>
+      </ul>
 
       <input type="submit" />
     </form>
